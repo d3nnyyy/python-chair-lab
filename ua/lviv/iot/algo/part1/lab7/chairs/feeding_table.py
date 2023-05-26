94f1ca96df34189ed4f891c0357d8dc2a7da4cc1
d3nnyyy <d3nnn41k@gmail.com>
2023-05-26 19:52:10 +0300

    adding files

---
+++ ua/lviv/iot/algo/part1/lab7/chairs/feeding_table.py
@@ -0,0 +1,44 @@
+"""
+Module for the FeedingTable class.
+"""
+from abc import ABC
+
+from ua.lviv.iot.algo.part1.lab7.chairs.chair import Chair
+
+
+class FeedingTable(Chair, ABC):
+    """
+    A class representing a feeding table chair.
+    Attributes:
+        MIN_HEIGHT (float): The minimum height of the feeding table.
+        MAX_HEIGHT (float): The maximum height of the feeding table.
+        max_weight (float): The maximum weight the chair can handle.
+        material (str): The material the chair is made of.
+        height (float): The height of the feeding table.
+        child_age (int): The age of the child that will use the feeding table.
+    """
+
+    MIN_HEIGHT = 0.5
+    MAX_HEIGHT = 1
+
+    def __init__(self, max_weight, material, height, child_age):
+        super().__init__(max_weight, material)
+        self.height = height
+        self.child_age = child_age
+
+    def __str__(self):
+        return f"FeedingTable: {self.max_weight}, {self.material}, {self.height}, {self.child_age}"
+
+    def adjust_position(self, delta_height):
+        """
+        Adjusts the height of the feeding table.
+        :arg delta_height: The amount of height to adjust the feeding table by.
+        :type delta_height: float
+        :raises ValueError: If the height of the feeding table is out of bounds.
+        """
+        if self.MIN_HEIGHT <= self.height + delta_height <= self.MAX_HEIGHT:
+            self.height += delta_height
+        elif self.height + delta_height > self.MAX_HEIGHT:
+            self.height = self.MAX_HEIGHT
+        else:
+            self.height = self.MIN_HEIGHT
---
+++ ua/lviv/iot/algo/part1/lab7/chairs/recliner_chair.py
@@ -0,0 +1,47 @@
+"""
+Module that contains the ReclinerChair class.
+"""
+from abc import ABC
+
+from ua.lviv.iot.algo.part1.lab7.chairs.chair import Chair
+
+
+class ReclinerChair(Chair, ABC):
+    """
+    A class representing a recliner chair.
+    Attributes:
+        MIN_ANGLE (float): The minimum angle of the recliner chair.
+        MAX_ANGLE (float): The maximum angle of the recliner chair.
+        max_weight (float): The maximum weight the chair can handle.
+        material (str): The material the chair is made of.
+        angle (float): The angle of the recliner chair.
+        has_remote_control (bool): Whether the recliner chair has a remote control.
+    """
+    MIN_ANGLE = 90
+    MAX_ANGLE = 150
+
+    def __init__(self, max_weight, material, angle, has_remote_control):
+        super().__init__(max_weight, material)
+        self.angle = angle
+        self.has_remote_control = has_remote_control
+
+    def __str__(self):
+        return f"ReclinerChair: " \
+               f"{self.max_weight}, " \
+               f"{self.material}, " \
+               f"{self.angle}, " \
+               f"{self.has_remote_control}"
+
+    def adjust_position(self, delta_angle):
+        """
+        Adjusts the angle of the recliner chair.
+        :param delta_angle: The amount of angle to adjust the recliner chair by.
+        :type delta_angle: float
+        :raises ValueError: If the angle of the recliner chair is out of bounds.
+        """
+        if self.MIN_ANGLE <= self.angle + delta_angle <= self.MAX_ANGLE:
+            self.angle += delta_angle
+        elif self.angle + delta_angle > self.MAX_ANGLE:
+            self.angle = self.MAX_ANGLE
+        else:
+            self.angle = self.MIN_ANGLE
